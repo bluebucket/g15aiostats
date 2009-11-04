310ab145efa32c513ffed54bb64dfd5d925956ae
kbranch@kbranch.net <kbranch@kbranch.net@a493ba14-c968-11de-8f3f-2f730f80caef>
2009-11-04 18:24:42 +0000

    -Initial commit

---
+++ trunk/src/common.h
@@ -0,0 +1,267 @@
+/***************************************************************************
+*   Copyright (C) 2009 by Steven Collins                                  *
+*   kbranch@kbranch.net                                                   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 3 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+***************************************************************************/
+
+#ifndef MAINFILE
+	#define EXTERN extern
+#else
+	#define EXTERN
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <math.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <boost/regex.hpp>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <linux/soundcard.h>
+
+#ifdef HAVE_LIBSENSORS
+	#include <sensors/sensors.h>
+#endif
+
+#if defined HAVE_LIBG15RENDER && defined HAVE_LIBG15DAEMON_CLIENT
+	#include <libg15.h>
+	#include <libg15render.h>
+	#include <g15daemon_client.h>
+#else
+	enum { G15_KEY_L2 = 1<<23, G15_KEY_L3 = 1<<24, G15_KEY_L4 = 1<<25,
+		G15_KEY_L5 = 1<<26 };
+#endif
+
+#ifdef HAVE_LIBGTOP_2_0
+	#include <glibtop/mem.h>
+	#include <glibtop/swap.h>
+	#include <glibtop/netload.h>
+	#include <glibtop/netlist.h>
+	#include <glibtop/mountlist.h>
+	#include <glibtop/fsusage.h>
+#else
+	typedef unsigned long long int guint64;
+	typedef long long int gint64;
+#endif
+
+#define FRAME_TIME 50000
+#define SECTOR_SIZE 512
+#define MAXSCREENS 10
+#define MAXBARS 50
+#define MAXSETTINGS 10
+
+using namespace std;
+
+enum { CPU, NET, DOWNLOAD, UPLOAD, DISK, MEM, TIME, TIMER, SENSOR, UPTIME,
+	LOADAVG, DISKSPACE, SWAP, USERSCRIPT, VOLUME }; // Bar types
+enum { TEMP, FAN }; // Sensor types
+enum { LEFT, RIGHT, CENTER }; // Text justifications
+enum { SMALL_FONT, MEDIUM_FONT, LARGE_FONT }; // Font sizes
+
+struct Timer
+{
+	guint64 startTime;
+	guint64 stopTime;
+	int stopped;
+	int processed;
+	int reset;
+};
+
+struct ScreenText
+{
+	int x;
+	int y;
+	int xMod;
+	int yMod;
+	int xFixed;
+	int yFixed;
+	int just;
+	string text;
+};
+
+struct Bar
+{
+	double values[MAXSETTINGS];
+	double speeds[MAXSETTINGS];
+	double max[MAXSETTINGS];
+	double samples[MAXSETTINGS][2];
+	guint64 tempData[MAXSETTINGS][5];
+	ScreenText str[MAXSETTINGS];
+	ScreenText label;
+	string timeFormat;
+	string stringFormat[MAXSETTINGS];
+	int x;
+	int y;
+	int xMod;
+	int yMod;
+	int xFixed;
+	int yFixed;
+	int h;
+	int w;
+	int wMod;
+	int wFixed;
+	int numStrings;
+	int fontSize;
+	int numSubBars;
+	int type;
+	int hasBar;
+	int line;
+	int linePop;
+	int sectionNum;
+	int sensorChip[MAXSETTINGS];
+	int sensorType[MAXSETTINGS];
+	int sensorNum[MAXSETTINGS];
+	int numSensors;
+	int buttons[MAXSETTINGS];
+	int hideButton[MAXSETTINGS];
+	int numButtons;
+	int isLocal;
+	int oldLeftWidth;
+	int oldRightWidth;
+	int oldCenterWidth[MAXSETTINGS];
+	int manualLayout;
+	FILE *fileHandle;
+	string sensorSpacer;
+	timeval sampleTime;
+	Timer timer;
+	int reset;
+	unsigned int device;
+	unsigned int numDevices;
+	int isStereo[50];
+	int isAvailable[50];
+	string devices[50];
+	string defaultDevice;
+	string clientName;
+	int socket;
+};
+
+struct Socket
+{
+	int fd;
+	int connected;
+	int connections[10];
+	int lastError;
+	guint64 lastTry;
+	addrinfo *res;
+	string address;
+	string port;
+	string clientNames[10];
+	
+	int numBars;
+	Bar bars[MAXBARS];
+	unsigned int keyState;
+};
+
+struct Screen
+{
+	int numBars;
+	int autoLayout;
+	int button;
+	string lStrings[4];
+	Bar *bars;
+};
+
+EXTERN int autoLayout;
+EXTERN int daemonize;
+EXTERN int numBars;
+EXTERN int numScreens;
+EXTERN int currentScreen;
+EXTERN int screen;
+EXTERN int quiet;
+EXTERN int fontHeights[3];
+EXTERN int fontWidths[3];
+EXTERN int lastOutErrors[10];
+EXTERN int lastInErrors[10];
+EXTERN string lStrings[4];
+EXTERN string configPath;
+EXTERN string myName;
+EXTERN Socket inSocket;
+EXTERN Socket outSockets[10];
+EXTERN guint64 rotateDelay;
+const string typeStrings[15] = { "CPU", "NET", "DOWNLOAD", "UPLOAD", "DISK",
+	"MEM", "TIME", "TIMER", "SENSOR", "UPTIME", "LOADAVG", "DISKSPACE", "SWAP",
+	"USERSCRIPT", "VOLUME" };
+
+EXTERN Screen screens[MAXSCREENS];
+
+#if defined HAVE_LIBG15RENDER && defined HAVE_LIBG15DAEMON_CLIENT
+EXTERN g15canvas canvas;
+#endif
+
+Bar *Init();
+void InitBar(Bar &bar);
+void InitButtonStrings(Bar *bars);
+void InitNetwork();
+int LoadConfig(string configPath, int silent);
+void WriteConfig(string configFile);
+void LayoutBars(Bar *bars);
+Bar *CheckInput(Bar *bars);
+void OutputReport(Bar *bars);
+void UpdateBars(Bar *bars);
+void UpdateNetUsage(Bar &usage, int updateStrings);
+void UpdateCpuUsage(Bar &usage, int updateStrings);
+void UpdateMemUsage(Bar &usage, int updateStrings);
+void UpdateSwapUsage(Bar &usage, int updateStrings);
+void UpdateDiskUsage(Bar &usage, int updateStrings);
+void UpdateSensors(Bar &bar, int updateStrings);
+void UpdateUptime(Bar &bar, int updateStrings);
+void UpdateLoadAvg(Bar &bar, int updateStrings);
+void UpdateDiskSpace(Bar &bar, int updateStrings);
+void UpdateVolume(Bar &bar, int updateStrings);
+void UpdateTime(Bar &bar);
+void UpdateTimer(Bar &bar);
+void UpdateUserScript(Bar &bar);
+string ResizeNum(double num, string suffix);
+unsigned char* ftoa(double number, int precision);
+unsigned char* itoa(gint64 number, int width, char fill);
+guint64 GetCurrentTime();
+timeval ConvertTimeFormat(guint64 time);
+void HandleOptions(int argc, char *argv[]);
+void HandleSignal(int signal);
+double ParseUnits(string &text, string format, double number);
+double ParseUnits(string text);
+int CheckButton(int button, unsigned int keyState);
+Bar *ChangeScreen(int screen);
+Socket CreateSocket(const char *address, const char *port);
+int ConnectSocket(Socket &sock);
+int AcceptConnection(Socket &sock);
+int ListenSocket(Socket &sock);
+int SendMessage(const char *message, int fd);
+string ReceiveMessage(int fd);
+void CheckNetwork(guint64 timeout);
+int Handshake(int fd);
+int SendNeedString(int fd, string clientName);
+int SendNetworkBars();
+void ParseMessage(string message, Socket &socket, int num);
+void RepositionBar(Bar &bar);
+int FindNextLine(int lineIndices[MAXBARS], int index);
+void CheckBarInput(Bar &bar, unsigned int keyState, int updateLocalStrings);
